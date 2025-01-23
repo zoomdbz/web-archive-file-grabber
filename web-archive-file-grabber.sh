e2b91ae53d2802bc5fbe5ec4a3db7a4f14779cc3
Dividesbyzer0 <54127744+zoomdbz@users.noreply.github.com>
2025-01-23 18:37:11 -0500

    Update web-archive-file-grabber.sh

--- web-archive-file-grabber.sh
+++ web-archive-file-grabber.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-curl -s "https://web.archive.org/cdx/search/cdx?url=*${1}/*&collapse=urlkey&output=text&fl=original" > scrape.txt && grep -Eo "\.(zip|rar|7z|docx?|xlsx?|pptx?|pdf|json|txt|csv|log|xml|sql|yaml|yml|ini|cfg|conf|bat|ps1|sh|php|rb|py|java|c|cpp|cs|rtf|dat|bak|tar|gz|pem|key|cert|pfx|db|mdb|sqlite|accdb)\b" scrape.txt > file-matches.txt
+curl -s "https://web.archive.org/cdx/search/cdx?url=*.${1}/*&collapse=urlkey&output=text&fl=original" > scrape.txt && grep -E "https?://[^ ]+\.(zip|rar|7z|docx?|xlsx?|pptx?|pdf|json|txt|csv|log|xml|sql|yaml|yml|ini|cfg|conf|env|dat|bak|tar|gz|tgz|pem|key|crt|cer|pfx|db|sqlite|accdb|mdb|py|rb|php|js|java|cpp|sh|ps1|bat|cmd|dockerfile|tf|tfstate|kubeconfig)\b
+" scrape.txt > file-matches.txt
